2f7dc954eeec359f3934b3447de1ea1957569f02
nadja.karla_@hotmail.com <nadja.karla_@hotmail.com>
2020-11-09 16:33:59 -0300

    att q1

--- q1.py
+++ q1.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-from PIL import Image
+from PIL import Image as pil
 
 def RGBtoYIQ():
-    img = mpimg.imread("q1Test/Detran_Minas-Gerais.jpg") #carrega a imagem como um array de pixels
+    img = mpimg.imread("q1Test/Detran_Minas-Gerais.jpg") #o imreadcarrega a imagem como um array de pixels
     plt.imshow(img.astype('uint8'))
     plt.axis('off')
     plt.figure()
@@ -14,15 +14,17 @@ def RGBtoYIQ():
                 [ 0.587, - 0.274, - 0.523 ],
                 [ 0.114, - 0.322 ,0.312 ]]) 
 
-    
     #mutiplicação das matrizes
     rgbToYiq = np.dot(img/255,yiq)
 
-    plt.imshow(rgbToYiq)
-    plt.axis('off')
-    plt.figure()
+    x = rgbToYiq
+    plt.imsave('q1Test/imagemyiq.png' , np.asarray(np.array((x - np.min(x)) / (np.max(x) - np.min(x)))) )
 
-    return rgbToYiq           
+    #plt.imshow(np.asarray(np.array((x - np.min(x)) / (np.max(x) - np.min(x)))) )
+    #plt.axis('off')
+    #plt.figure()
+
+    return x          
 
 def YIQtoRGB(rgbToYiq):
 
@@ -32,19 +34,21 @@ def YIQtoRGB(rgbToYiq):
 
     rgb = np.linalg.inv(yiq)
     
-    yiqToRgb = np.dot(rgbToYiq * 255,rgb)
+    yiqToRgb = np.dot(rgbToYiq *255,rgb)
     
     yiqToRgb[yiqToRgb > 255] = 255
     yiqToRgb[yiqToRgb < 0]= 0
 
-    plt.imshow(yiqToRgb.astype('uint8'))
-    plt.axis('off')  
-    plt.show()
+    y = yiqToRgb
+    plt.imsave('q1Test/imagemrgb.png' , np.asarray(np.array((y - np.min(y)) / (np.max(y) - np.min(y)))) )
+
+    #plt.imshow(np.asarray(np.array((y - np.min(y)) / (np.max(y) - np.min(y)))))
+    #plt.axis('off')  
+    #plt.show()
     
+#-----------------Visualização das imagens-----------------------#
 
-""" #-----------------Visualização das imagens-----------------------#
-
-filename = ['q1Test/Detran_Minas-Gerais.jpg', 'q1Test/new.jpg', 'q1Test/new2.jpg']
+filename = ['q1Test/Detran_Minas-Gerais.jpg', 'q1Test/imagemyiq.png', 'q1Test/imagemrgb.png']
 titles = ['Original', 'RGB to YIQ', 'YIQ to RGB']
 fig=plt.figure(figsize=(8, 8)) #define o tamanho que as figuras terão na janela
 
@@ -62,7 +66,7 @@ for i in range(columns*rows):
     plt.axis('off') #tira a amostragem dos eixos x e y das imagens
 
 plt.show() #inicia o loop pra abertura da janela'''
- """
+ 
 
 #Pra ver individualmente """
 '''
