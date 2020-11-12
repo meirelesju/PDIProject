8c582acd3b65454df62c67c2e1c8028ee9fa9f95
nadja.karla_@hotmail.com <nadja.karla_@hotmail.com>
2020-11-12 14:49:37 -0300

    att q1

--- q1.py
+++ q1.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from PIL import Image
 
 def RGBtoYIQ():
     img = mpimg.imread("q1Test/Detran_Minas-Gerais.jpg") #o imreadcarrega a imagem sendo um array de pixels
-    plt.imshow(img.astype('uint8'))
-    plt.axis('off')
-    plt.figure()
 
     #mascara yiq
     yiq =np.array([[ 0.299 , 0.596,  0.211 ],
@@ -14,16 +12,8 @@ def RGBtoYIQ():
                 [ 0.114, - 0.322 ,0.312 ]]) 
 
     #mutiplicação das matrizes
-    rgbToYiq = np.dot(img/255,yiq)
-
-    x = rgbToYiq
-    #isso é pra deixar os valores nos limites pra poder salvar a imagem yiq
-    plt.imsave('q1Test/imagemyiq.png' , np.asarray(np.array((x - np.min(x)) / (np.max(x) - np.min(x)))))
-    #plt.imshow(np.asarray(np.array((x - np.min(x)) / (np.max(x) - np.min(x)))) )
-    #plt.axis('off')
-    #plt.figure()
-
-    return x          
+    rgbToYiq = np.dot(img/255,yiq) # divide por 255 para ficar com valores entre 0 e 1
+    return rgbToYiq        
 
 def YIQtoRGB(rgbToYiq):
     #mascara yiq
@@ -33,27 +23,25 @@ def YIQtoRGB(rgbToYiq):
                 
     #o "inv" server para inverter a matriz yiq pra virar uma matriz rgb
     rgb = np.linalg.inv(yiq)
-    
-    yiqToRgb = np.dot(rgbToYiq *255,rgb)
-    
-    yiqToRgb[yiqToRgb > 255] = 255
-    yiqToRgb[yiqToRgb < 0]= 0
 
-    y = yiqToRgb
-    plt.imsave('q1Test/imagemrgb.png')
-
-    #plt.imshow(np.asarray(np.array((y - np.min(y)) / (np.max(y) - np.min(y)))))
-    #plt.axis('off')  
-    #plt.show()
+    yiqToRgb = np.dot(rgbToYiq,rgb) 
     
+    yiqToRgb[yiqToRgb > 1] = 1
+    yiqToRgb[yiqToRgb < 0] = 0
+    plt.imsave( 'q1Test/imagemrgb.png', yiqToRgb)
+   
+
 #-----------------Visualização das imagens-----------------------#
 
-filename = ['q1Test/Detran_Minas-Gerais.jpg', 'q1Test/imagemyiq.png', 'q1Test/imagemrgb.png']
-titles = ['Original', 'RGB to YIQ', 'YIQ to RGB']
-fig=plt.figure(figsize=(8, 8)) #define o tamanho que as figuras terão na janela
+yiq = RGBtoYIQ()
+YIQtoRGB(yiq)
+
+filename = ['q1Test/Detran_Minas-Gerais.jpg','q1Test/imagemrgb.png']
+titles = ['Original', 'YIQ to RGB']
+fig=plt.figure(figsize=(7, 7)) #define o tamanho que as figuras terão na janela
 
 #define quantas linhas e colunas de imagem deverão aparecer na mesma janela
-columns = 3
+columns = 2
 rows = 1
 
 figuras=[] #usaremos p/ separar cada imagem p/ poder organiza-las individualmente
@@ -66,6 +54,3 @@ for i in range(columns*rows):
     plt.axis('off') #tira a amostragem dos eixos x e y das imagens
 
 plt.show() #inicia o loop pra abertura da janela'''
-
-Rj = RGBtoYIQ()
-YIQtoRGB(Rj)
